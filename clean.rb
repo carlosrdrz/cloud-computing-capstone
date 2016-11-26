24bbc9b01f6fe81a22cf308e7f87c4167e1dfb2e
Carlos Rodriguez <crresse@gmail.com>
2016-11-26 23:11:27 +0000

    Added exceptions handling to clean script

--- clean.rb
+++ clean.rb
@@ -10,18 +10,23 @@ keys = ARGV[2].split(',')
 
 files = Dir["#{source_dir}/**/*.zip"]
 Parallel.map(files, in_processes: 14) do |file|
-  Zip::File.open(file) do |zip_file|
-    zip_file.glob('*.csv').each do |csv_file_entry|
-      csv_file = csv_file_entry.get_input_stream.read
+  puts file
+
+  begin
+    Zip::File.open(file) do |zip_file|
+      zip_file.glob('*.csv').each do |csv_file_entry|
+        csv_file = csv_file_entry.get_input_stream.read
       
-      File.open("#{dest_dir}/#{csv_file_entry.name}", 'a') do |f|
-        puts csv_file_entry.name
-        CSV.parse(csv_file, headers: true) do |row|
-          values = keys.map { |k| row[k] }
-          f.write "#{values.join(',')}\n"
+        File.open("#{dest_dir}/#{csv_file_entry.name}", 'a') do |f|
+          CSV.parse(csv_file, headers: true) do |row|
+            values = keys.map { |k| row[k] }
+            f.write "#{values.join(',')}\n"
+          end
         end
       end
     end
+  rescue StandardError => e
+    puts "[ERROR] #{file} #{e.message}"
   end
 end
 
